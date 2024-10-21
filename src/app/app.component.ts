c70da53f02fe2a114573bfdb8534b9c5aefa0d64
Rafael <rafaeldecastro.dev@gmail.com>
2024-10-21 19:32:08 -0300

    Curriculo pronto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ButtonGroupModule } from 'primeng/buttongroup';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ButtonGroupModule, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'aboutme';
+  selectedLang: string = 'en';
+
+  changeLang(lang: string) {
+    this.selectedLang = lang;
+  }
 }
